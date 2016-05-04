a0986cea202767bead52f30f1aacf46c55ba9975
MatR <matrphone@gmail.com>
2016-05-04 18:11:27 +0200

    no message

---
+++ src/jsfml1/JSFML1.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jsfml1;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import org.jsfml.audio.*;
+import sun.net.ftp.FtpClient;
+import sun.net.www.protocol.ftp.FtpURLConnection;
+
+/**
+ *
+ * @author matr
+ */
+public class JSFML1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws IOException {
+        // TODO code application logic here
+        new NewJFrame().setVisible(true);        
+        
+    }
+    
+}
